73398fc7e8b173f5c4b6e7cd65918e1a99c99d75
Your Name <jideman2k@yandex.ru>
2025-02-03 11:07:40 -0800

    update css

--- src/utils/variable.js
+++ src/utils/variable.js
@@ -31,7 +31,7 @@ export const projectsData = [
   {
     type: "bg1",
     value: "16 <span>| 24</span>",
-    label: "Goals Completed",
+    label: "Recipe App",
     details:
       "Vanilla JS project with a focus on creating a visually appealing and interactive user interface.",
     stacks: ["HTML5", "CSS3", "JavaScript"],
@@ -41,9 +41,9 @@ export const projectsData = [
   {
     type: "bg1",
     value: '<i class="fas fa-battery-three-quarters"></i>',
-    label: "Respiration",
+    label: "Health Tracker",
     details:
-      "Respiration is a simple web application that allows users to track their breathing patterns and monitor their respiratory health.",
+      " simple web application that allows users to track their breathing patterns and monitor their respiratory health.",
     stacks: ["HTML5", "CSS3", "JavaScript", "Webpack"],
     gitLink: "https://github.com/cursorbot/cursorbot",
     image: heroImg,
@@ -51,26 +51,26 @@ export const projectsData = [
   {
     type: "bg2",
     value: '<i class="fas fa-running"></i>',
-    label: "Miles",
+    label: "React App",
     details: "A simple js application which build on webppack and react.",
-    stacks: ["HTML5", "CSS3", "JavaScript", "Webpack", "NextJS"],
+    stacks: ["HTML5", "CSS3", "JavaScript", "Webpack", "React"],
     gitLink: "https://github.com/cursorbot/cursorbot",
     image: heroImg,
   },
   {
     type: "bg1",
     value: "36 &deg;",
-    label: "Temperature",
+    label: "Fullstack Recipe App",
     details:
       "A fullstack web application that allows to create and vue your favorite recipes.",
-    stacks: ["HTML5", "CSS3", "JavaScript", "Webpack", "VueeJS"],
+    stacks: ["HTML5", "CSS3", "JavaScript", "Webpack", "VueJS"],
     gitLink: "https://github.com/cursorbot/cursorbot",
     image: heroImg,
   },
   {
     type: "bg1",
     value: '<i class="fas fa-bed"></i>',
-    label: "Sleep Keep",
+    label: "Todo note app",
     details:
       "Demostrate how to use the HTML5 and CSS3 to create a responsive layout.",
     stacks: ["HTML5", "CSS3", "JavaScript"],
@@ -80,7 +80,7 @@ export const projectsData = [
   {
     type: "bg2",
     value: "98 <span>bpm</span>",
-    label: "Heart Rate",
+    label: "Portfolio template",
     details:
       "crafted like a js framework single page application without using any framework.",
     stacks: ["HTML5", "CSS3", "JavaScript"],
