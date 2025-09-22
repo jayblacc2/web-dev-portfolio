42eb743d583dac77adebb7a9bf7aacc4158e2357
jayblaq <jideman2k@yandex.ru>
2025-09-22 03:03:19 -0700

    Refactor loadSection.js: update imports and section loading

--- src/modules/loadSection.js
+++ src/modules/loadSection.js
@@ -1,12 +1,12 @@
 import homeSection from "../sections/home";
-import aboutSection, { ProgressBar } from "../sections/about";
+import aboutSection from "../sections/about";
 import contactSection from "../sections/contact";
 import skillSection from "../sections/skill";
 import projectSection from "../sections/projects";
-import { createElement, selectAll } from "../utils/utils";
+import { createElement } from "../utils/utils";
 
 function loadSection() {
-  const content = createElement("div", { id: "content", class: "content" });
+  const content = createElement("main", { id: "content", class: "content" });
   const sections = [
     homeSection,
     aboutSection,
@@ -14,9 +14,7 @@ function loadSection() {
     projectSection,
     contactSection,
   ];
-  sections.forEach((section) => {
-    content.append(section());
-  });
+  sections.forEach((section) => content.append(section()));
 
   return content;
 }
